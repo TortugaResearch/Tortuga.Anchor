77903151e39b192b01aad65410d930a4376b5f87
Jonathan Allen <jonathanallen@kpmg.com>
2016-03-20 23:27:03 -0700

    Tested nuget packages.

---
+++ Tortuga.Anchor/PackageTest.Tortuga.Anchor.net461/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageTest.Tortuga.Anchor.net461
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
