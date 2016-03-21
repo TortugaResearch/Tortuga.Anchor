48bbe1a6b870fddd901fce5d4a647fb15c657f72
Jonathan Allen <jonathanallen@kpmg.com>
2016-03-20 23:28:08 -0700

    Missing file

---
+++ Tortuga.Anchor/Tortuga.Anchor.uap10/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tortuga.Anchor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
