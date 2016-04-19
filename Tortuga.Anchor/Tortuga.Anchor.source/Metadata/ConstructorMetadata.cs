be2dc57ea2843fc414cef79984962502b96909ea
Jonathan Allen <jonathanallen@kpmg.com>
2016-04-18 17:20:54 -0700

    Constructor metadata

---
+++ Tortuga.Anchor/Tortuga.Anchor.source/Metadata/ConstructorMetadata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+
+#if !DataAnnotations_Missing
+#endif
+
+namespace Tortuga.Anchor.Metadata
+{
+
+
+    /// <summary>
+    /// Class ConstructorMetadata.
+    /// </summary>
+    public sealed class ConstructorMetadata
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructorMetadata"/> class.
+        /// </summary>
+        /// <param name="constructorInfo">The constructor information.</param>
+        internal ConstructorMetadata(ConstructorInfo constructorInfo)
+        {
+            ConstructorInfo = constructorInfo;
+
+            Signature = constructorInfo.GetParameters().Select(p => p.ParameterType).ToImmutableArray();
+        }
+
+        /// <summary>
+        /// Gets the signature.
+        /// </summary>
+        /// <value>The signature.</value>
+        public ImmutableArray<Type> Signature { get; }
+
+        /// <summary>
+        /// Gets the constructor information.
+        /// </summary>
+        /// <value>The constructor information.</value>
+        public ConstructorInfo ConstructorInfo { get; }
+    }
+
+        
+}
