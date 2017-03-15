c75a0a36d007feaea2f19318ca04a94da5abb842
Jonathan Allen <jonathanallen@kpmg.com>
2017-03-15 00:42:30 -0700

    Tortuga.Anchor.1.2.6283.989

--- Tortuga.Anchor/Tortuga.Anchor.netstandard1.3/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Tortuga.Anchor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration(".NET Standard 1.3")]
-[assembly: ComVisible(false)]
-[assembly: Guid("ee5bc9f0-7141-49d2-95ce-81cdbcfc2885")]
--- Tortuga.Anchor/Tortuga.Anchor.source/Properties/LibrarySharedAssemblyInfo.cs
+++ Tortuga.Anchor/Tortuga.Anchor.source/Properties/LibrarySharedAssemblyInfo.cs
@@ -1,8 +1,11 @@
 ﻿using System.Reflection;
 
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if !NETSTANDARD1_3
+[assembly: AssemblyVersion("1.2.*")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tortuga Anchor")]
 [assembly: AssemblyCopyright("Copyright © 2016 David Tomcik, Jonathan Allen")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.*")]
+#endif
