5cc26c3e8e5deff5c2491702562987f9882f90cf
Jonathan Allen <grauenwolf@gmail.com>
2019-04-03 10:23:49 -0700

    Fixed assembly info.

--- Tortuga.Anchor/Tortuga.Anchor.source/Properties/AssemblyInfo.cs
+++ Tortuga.Anchor/Tortuga.Anchor.source/Properties/AssemblyInfo.cs
@@ -1,11 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tortuga.Anchor")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("500b9fb8-546f-4f84-95a7-02f2acaa2ffd")]
-
