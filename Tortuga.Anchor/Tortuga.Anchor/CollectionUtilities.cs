a4755205924aff1e56980c7f221892ddaf336125
Jonathan Allen <grauenwolf@gmail.com>
2020-12-05 22:01:14 -0800

    Added missing NotNullIfNotNull to IReadOnlyList Added missing ? to AsList

--- Tortuga.Anchor/Tortuga.Anchor/CollectionUtilities.cs
+++ Tortuga.Anchor/Tortuga.Anchor/CollectionUtilities.cs
@@ -104,7 +104,7 @@ namespace Tortuga.Anchor
         /// disguised as IEnumerable.
         /// </remarks>
         [return: NotNullIfNotNull("source")]
-        public static IList<T>? AsList<T>(this IEnumerable<T> source)
+        public static IList<T>? AsList<T>(this IEnumerable<T>? source)
         {
             if (source == null)
                 return null;
@@ -141,6 +141,7 @@ namespace Tortuga.Anchor
         /// <param name="source">The source. If the source is null, the result will be null.</param>
         /// <returns>IReadOnlyList&lt;T&gt;.</returns>
         /// <remarks>This is meant to be used for legacy codebases that predate IReadOnlyList&lt;T&gt;.</remarks>
+        [return: NotNullIfNotNull("source")]
         public static IReadOnlyList<T>? AsReadOnlyList<T>(this IEnumerable<T>? source)
         {
             if (source == null)
