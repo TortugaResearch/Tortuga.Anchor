f272170a8412deaaf5c8711db2950bfbf3bbc406
Jonathan Allen <grauenwolf@gmail.com>
2022-07-21 20:52:29 -0700

    Prep for v4.2 Remove unnecessary compiler constants.

--- Tortuga.Anchor/Tortuga.Anchor/Legacy/HashCode.cs
+++ Tortuga.Anchor/Tortuga.Anchor/Legacy/HashCode.cs
@@ -1,22 +1,15 @@
-﻿#if HASHCODE_MISSING
+﻿#if !NETCOREAPP3_1_OR_GREATER
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace System;
 
-namespace System
+//Not the most elegant hash code function, but it is only needed for down-level platforms.
+
+static class HashCode
 {
-    //Not the most elegant hash code function, but it is only needed for down-level platforms.
-
-    static class HashCode
-    {
-        internal static int Combine(int a, int b, int c)
-        {
-            return a ^ b ^ c;
-        }
-    }
+	internal static int Combine(int a, int b, int c)
+	{
+		return a ^ b ^ c;
+	}
 }
 
 #endif
---
+++ Tortuga.Anchor/Tortuga.Anchor/Legacy/IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿#if !NET5_0_OR_GREATER
+
+namespace System.Runtime.CompilerServices;
+
+static class IsExternalInit
+{
+}
+
+#endif
