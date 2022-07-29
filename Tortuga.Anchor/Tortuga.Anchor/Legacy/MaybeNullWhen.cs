c22dd06d9dcaec5fa4e8d79a835e4b7ad72ae733
Jonathan Allen <grauenwolf@gmail.com>
2022-07-28 22:30:10 -0700

    MultiValueDictionary Class #85

---
+++ Tortuga.Anchor/Tortuga.Anchor/Legacy/MaybeNullWhen.cs
@@ -0,0 +1,16 @@
+﻿#if !NETCOREAPP3_1_OR_GREATER
+
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+internal sealed class MaybeNullWhen : Attribute
+{
+	public MaybeNullWhen(bool returnValue)
+	{
+		ReturnValue = returnValue;
+	}
+
+	public bool ReturnValue { get; }
+}
+
+#endif
