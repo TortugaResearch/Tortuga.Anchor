39975a578597943d76e176f57b01c3254f923dbf
Jonathan Allen <grauenwolf@gmail.com>
2022-07-29 18:49:11 -0700

    Preferred Constructor Attribute #65

--- Tortuga.Anchor/Tortuga.Anchor/Metadata/ConstructorMetadata.cs
+++ Tortuga.Anchor/Tortuga.Anchor/Metadata/ConstructorMetadata.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Immutable;
 using System.Reflection;
+using Tortuga.Anchor.Modeling;
 
 namespace Tortuga.Anchor.Metadata;
 
@@ -18,13 +19,28 @@ public sealed class ConstructorMetadata
 
 		Signature = constructorInfo.GetParameters().Select(p => p.ParameterType).ToImmutableArray();
 		ParameterNames = constructorInfo.GetParameters().Select(p => p.Name).Cast<string>().ToImmutableArray();
+
+		Attributes = ImmutableArray.CreateRange(Attribute.GetCustomAttributes(constructorInfo, true));
+
+		IsPreferred = Attributes.OfType<PreferredConstructorAttribute>().Any();
 	}
+
+	/// <summary>
+	/// Complete list of attributes that apply to the constructor.
+	/// </summary>
+	/// <remarks>This does not include parameter level attributes.</remarks>
+	public ImmutableArray<Attribute> Attributes { get; }
 
 	/// <summary>
 	/// Gets the constructor information.
 	/// </summary>
 	/// <value>The constructor information.</value>
 	public ConstructorInfo ConstructorInfo { get; }
+
+	/// <summary>
+	/// Gets a value indicating whether this is the preferred constructor.
+	/// </summary>
+	public bool IsPreferred { get; }
 
 	/// <summary>
 	/// Gets the parameter names.
